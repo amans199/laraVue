9a96ad16d4d6fd7d92bc142d219269963229c1ac
amans199 <ahmed.ouda1997@gmail.com>
2020-06-02 08:26:21 +0200

    fix styles in print page

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,13 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/invoice', function () {
+    return view('invoice');
+});
+
 
 Route::get('/{path}', 'HomeController@index')->where('path', '([A-z\d-\/_.]+)?');
